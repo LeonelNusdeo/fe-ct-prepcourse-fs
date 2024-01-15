00dadc26085942595e98f137ce63ea4629061e0c
Leonel Nusdeo <leonel.nusdeo@gmail.com>
2024-01-15 10:53:57 -0300

    Agregado el Homework de Modulo 03 y Modulo 04 (anteriormente en otro repositorio). Resueltos ejercicios Modulo 05 del 01-10, 12, 25-27

--- M05 Fundamentos Javascript/ejercicios/10.js
+++ M05 Fundamentos Javascript/ejercicios/10.js
@@ -3,6 +3,11 @@ function esFechaValida(fecha) {
   // Comprueba si este corresponde a una fecha válida.
   // Si es así, retorna true, sino retorna false.
   // Tu código:
+  return fecha instanceof Date && !isNaN(fecha);
+  // instanceof https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/instanceof
+  // https://stackoverflow.com/questions/1353684/detecting-an-invalid-date-date-instance-in-javascript
+  // no valido si esta mal pasado el string: YYYY-MM-DD o MM-DD-YYYY son los validos
+  // DD-MM-YYYY es invalido (hay que parsearlo)
 }
 
 module.exports = esFechaValida;
