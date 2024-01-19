90819f28aa11c66c9600aec8d7dcbecc1819023e
Leonel Nusdeo <leonel.nusdeo@gmail.com>
2024-01-19 17:42:04 -0300

    Resueltos ejercicios Modulo 08 CB 04-08

--- M08 Objects + Callbacks/callbacks/ejercicios/06.js
+++ M08 Objects + Callbacks/callbacks/ejercicios/06.js
@@ -3,6 +3,12 @@ function map(array, cb) {
   // Tiene que guardar el resultado devuelto por el callback en cada elemento dentro de un nuevo arreglo.
   // Retorna el nuevo arreglo.
   // Tu código:
+  var nuevoArreglo = [];
+  array.forEach((num) => {
+    let valor = cb(num);
+    nuevoArreglo.push(valor);
+  });
+  return nuevoArreglo;
 }
 
 module.exports = map;
--- M08 Objects + Callbacks/callbacks/ejercicios/07.js
+++ M08 Objects + Callbacks/callbacks/ejercicios/07.js
@@ -2,6 +2,12 @@ function filter(arrayOfStrings) {
   // Debes identificar todos los elementos el arreglo que comiencen con la letra "a".
   // Luego retorna un nuevo arreglo con estos elementos.
   // Tu código:
+  let empiezaConA = arrayOfStrings.filter((str, i, arr) =>{
+    if (str.charAt(0) == "a") {
+      return str;
+    }
+  });
+  return empiezaConA;
 }
 
 module.exports = filter;
